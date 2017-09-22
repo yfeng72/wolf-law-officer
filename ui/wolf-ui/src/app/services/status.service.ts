630e991bdba4523323745149a223de998197102f
maxpeng1 <maxpeng1@4RFJPF2>
2017-09-22 17:52:22 -0400

    Updates on UI

--- ui/wolf-ui/src/app/services/status.service.ts
+++ ui/wolf-ui/src/app/services/status.service.ts
@@ -7,15 +7,15 @@ import { CallService } from './call.service';
 @Injectable()
 export class StatusService {
 
-	_user: User;
+	_user: User = {
+        userId: -1,
+        identity: null,
+        isLawOfficer: false,
+        checkedIdentity: false
+      };
 
 	constructor(private callService: CallService) {
-		this._user = {
-	  		userId: -1,
-	  		identity: null,
-	  		isLawOfficer: false,
-	  		checkedIdentity: false
-  		};
+
 	}
 
 	getId(id: number) {
@@ -28,28 +28,30 @@ export class StatusService {
   		return this.callService.get(url).map(rsp => {});
 	}
 
-	checkIdentity() {
-		if (this._user.userId < 0) return;
+	checkIdentity(): Observable<string> {
 		let url = "getIdentity/" + this._user.userId;
-  		return this.callService.get(url).map(rsp => rsp as string).subscribe(rsp =>{
-  			this._user.identity = rsp;
-  		});
+  		return this.callService.get(url).map(rsp => rsp.json() as string);
 	}
 
 	checkNightInfo(): Observable<number[]> {
   		let url = "lastNightInfo/";
-  		return this.callService.get(url).map(rsp => rsp as number[]);
-  	}
+  		return this.callService.get(url).map(rsp => rsp.json() as number[]);
+  }
 
   	startGame(): Observable<boolean> {
   		let url = "startGame/";
-  		return this.callService.get(url).map(rsp => rsp as boolean);
+  		return this.callService.get(url).map(rsp => rsp.json() as boolean);
   	}
 
   	useSkill(targetId: number) {
-  		let url = "useSkill/" + this._user.userId + "/target/" + targetId;
+  		let url = "useSkill/?userId=" + this._user.userId + "&targetId=" + targetId;
   		return this.callService.get(url).map(rsp => {}).subscribe(() => {
   			console.log("skill used successfully");
   		});
+  	}
+
+  	reStart(): Observable<void> {
+  		let url = "reshuffle/";
+  		return this.callService.get(url).map(rsp => {});
   	}
 }
